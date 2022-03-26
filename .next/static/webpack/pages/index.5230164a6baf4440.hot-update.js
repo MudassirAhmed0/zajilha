992aacf37ab324b62b26ae582dd6c33275edaaaa
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-03-26 11:11:22 +0500

    origin

---
+++ .next/static/webpack/pages/index.5230164a6baf4440.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./assets/NewImages/index/banner/banner.jpg":
+/*!**************************************************!*\
+  !*** ./assets/NewImages/index/banner/banner.jpg ***!
+  \**************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/banner.614bd5f4.jpg\",\"height\":2996,\"width\":5760,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbanner.614bd5f4.jpg&w=8&q=70\"});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hc3NldHMvTmV3SW1hZ2VzL2luZGV4L2Jhbm5lci9iYW5uZXIuanBnLmpzIiwibWFwcGluZ3MiOiI7QUFBQSwrREFBZSxDQUFDLHFLQUFxSyIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvLi9hc3NldHMvTmV3SW1hZ2VzL2luZGV4L2Jhbm5lci9iYW5uZXIuanBnPzYxNzUiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL2Jhbm5lci42MTRiZDVmNC5qcGdcIixcImhlaWdodFwiOjI5OTYsXCJ3aWR0aFwiOjU3NjAsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGYmFubmVyLjYxNGJkNWY0LmpwZyZ3PTgmcT03MFwifTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./assets/NewImages/index/banner/banner.jpg\n");
+
+/***/ })
+
+});
